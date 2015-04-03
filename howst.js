5eef50a9d870639feb7deb6eafc0ff0a36f11c2a
gengel@medispend.com <gengel@medispend.com>
2015-04-03 11:05:58 -0400

    .

--- howst.js
+++ howst.js
@@ -18,108 +18,95 @@ program
   .version(pkg.version)
   .description(pkg.description)
 
-//Check required parameters
-var validated = false;
-if (program.args.length) validated = true;
+//Custom help text
+program
+  .on('--help', function(){
+    console.log(help.text);
+});
 
-if (validated) {
-  //Custom help text
-  program
-    .on('--help', function(){
-      console.log(help.text);
-  });
+//CREATE
+program
+  .command('create <hostname>')
+  .action(function(hostname, options) {
+    manage.newMachine(hostname)
+});
 
-  //CREATE
-  program
-    .command('create <hostname>')
-    .action(function(hostname, options) {
-      manage.newMachine(hostname)
-  });
+//CONFIG
+program
+  .command('config <hostname>')
+  .description("Reconfigure an existing Howst machine")
+  .action(function(hostname) {
+    manage.editMachine(hostname)
+});
 
-  //CONFIG
-  program
-    .command('config <hostname>')
-    .description("Reconfigure an existing Howst machine")
-    .action(function(hostname) {
-      manage.editMachine(hostname)
-  });
+//REMOVE
+program
+  .command('remove <hostname>')
+  .description("stops and deletes all traces of the Howst machine")
+  .action(function(hostname) {
+    manage.destroyMachine(hostname)
+});
 
-  //REMOVE
-  program
-    .command('remove <hostname>')
-    .description("stops and deletes all traces of the Howst machine")
-    .action(function(hostname) {
-      manage.destroyMachine(hostname)
-  });
+//LIST
+program
+  .command('list')
+  .description("Lists all known Howst machines")
+  .option('-s, --status', 'Show status')
+  .action(function(options) {
+    manage.listMachines(options);
+});
 
-  //LIST
-  program
-    .command('list')
-    .description("Lists all known Howst machines")
-    .option('-s, --status', 'Show status')
-    .action(function(options) {
-      manage.listMachines(options);
-  });
+//LIST
+program
+  .command('info <hostname>')
+  .description("Lists the properties of the given Howst machine")
+  .action(function(options) {
+    manage.getMachineInfo(options);
+    
+});
 
-  //LIST
-  program
-    .command('info <hostname>')
-    .description("Lists the properties of the given Howst machine")
-    .action(function(options) {
-      manage.getMachineInfo(options);
-      
-  });
+//Set the remote password
+program
+  .command('remote-password <password>')
+  .description("Sets password for syncing remote databases")
+  .action(function(password) {
+    process.env['HOWST_REMOTE_PASSWORD'] = password;
+});
 
-  //Set the remote password
-  program
-    .command('remote-password <password>')
-    .description("Sets password for syncing remote databases")
-    .action(function(password) {
-      process.env['HOWST_REMOTE_PASSWORD'] = password;
-  });
+/* TODO - find a way to have optional arguments with commander so we can use defaults
+//SET DEFAULT
+program
+  .command('default <hostname>')
+  .description("Sets the default Howst machine")
+  .action(function(machine) {
+    utils.setDefault(machine);
+});
+*/
 
+//We'll just pass anything else through to vagrant
+program
+  .command('*')
+  .description("Vagrant passthrough.  See below.")
+  .action(function () {
+    var parsedArgs = [];
+    for (var i = 0; i < arguments.length - 1; i++){
+      parsedArgs.push(arguments[i]);
+    }
+    var command = parsedArgs[0];
+    parsedArgs.shift();
+    parsedArgs.push(function(){});
+    if(utils.vagrantCommands.indexOf(command) == -1){
+      console.log("Invalid command.  Use 'howst -h' to see list of available commands.".red);
+      return;
+    }
+    vagrant[command].apply(this, parsedArgs);
+});
 
+program.parse(process.argv);
 
-  /* TODO - find a way to have optional arguments with commander so we can use defaults
-  //SET DEFAULT
-  program
-    .command('default <hostname>')
-    .description("Sets the default Howst machine")
-    .action(function(machine) {
-      utils.setDefault(machine);
-  });
-  */
-
-  //We'll just pass anything else through to vagrant
-  program
-    .command('*')
-    .description("Vagrant passthrough.  See below.")
-    .action(function () {
-      var parsedArgs = [];
-      for (var i = 0; i < arguments.length - 1; i++){
-        parsedArgs.push(arguments[i]);
-      }
-      var command = parsedArgs[0];
-      parsedArgs.shift();
-      parsedArgs.push(function(){});
-      if(utils.vagrantCommands.indexOf(command) == -1){
-        console.log("Invalid command.  Use 'howst -h' to see list of available commands.".red);
-        return;
-      }
-      vagrant[command].apply(this, parsedArgs);
-  });
-
-  program.parse(process.argv);
+if (!program.args.length) {
+  program.help()
 }
-else {
-  //Required parameters not met or --help invoked
-  //traditional method: program.help()
-  //Custom help text
-  console.log(help.text);
-
-
-}
-  
 
 /*
 //From a dir with a Vagrantfile, this will ssh into the VM 
